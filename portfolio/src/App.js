ff5e3f3afce0f5831c0d3d1b53bd2b2ee70d0a42
juan-josue <juansemail2001@gmail.com>
2024-09-09 23:21:10 -0400

    feat: routing

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
+import { Routes, Route, useLocation } from "react-router-dom";
+
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Experience from "./Experience";
+import Contact from "./Contact";
 
 function App() {
+  const location = useLocation();
+
   return (
-    <div className="App">
-      <h1 className="text-3xl text-blue-500 font-bold underline">Hello world!</h1>
-    </div>
+    <>
+      <Navbar />
+      <Routes location={location} key={location.pathname}>
+        <Route index element={<Home />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
